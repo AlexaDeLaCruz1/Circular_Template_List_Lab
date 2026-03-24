e2819ebc0dbcebc2e9c1192a9472bf6eb38d5992
AlexaDeLaCruz1 <alexa1019042@my.yosemite.edu>
2026-03-24 16:15:35 -0700

    Implement Podcast class with toString and play methods

---
+++ Podcast.cpp
@@ -0,0 +1,15 @@
+#include "Podcast.h"
+#include <sstream>
+// Override toString() from the Media base class.
+std::string Podcast::toString() const{
+    std::stringstream ss;
+    ss << "Podcast: " << episodeTitle
+       << " | Host: " << host
+       << " | Ep #: " << episodeNumber;
+    return ss.str();
+}
+
+// Override play() to provide Podcast-specific behavior.
+void Podcast::play() const {
+    std::cout << "🎙️ Resuming episode " << episodeNumber << ": " << episodeTitle << " 🎙️" << std::endl;
+}
